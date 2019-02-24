acd892cf3b81de8999f5f8faae1f57721d294573
Sergey Sergeev <adron@yapic.net>
2019-02-24 21:48:35 +0300

    add endians_conv flag

--- x_conf_reader.c
+++ x_conf_reader.c
@@ -13,9 +13,17 @@
 #define u16 uint16_t
 #define u32 uint32_t
 
+#define DO_ENDIANS_CONV 0
+
 static u32 get_u32(void *buf)
 {
+#if DO_ENDIANS_CONV == 1
+	const uint8_t *p = buf;
+	return ((uint32_t) p[3] + ((uint32_t) p[2] << 8) +
+	       ((uint32_t) p[1] << 16) + ((uint32_t) p[0] << 24));
+#else
 	return *(u32 *)buf;
+#endif
 }
 
 #define RB_BLOCK_SIZE		0x1000
